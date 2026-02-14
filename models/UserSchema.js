780a88ac187d33aa327c294b92ccc126a86bd522
vamshi-chikka <58411245+vamshi-chikka@users.noreply.github.com>
2026-02-14 22:34:04 +0530

    Add unique, lowercase, and trim properties to email

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -8,6 +8,9 @@ const userSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
     },
     mobile:{
         type:String,
